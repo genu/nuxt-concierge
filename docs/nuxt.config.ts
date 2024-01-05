0e5c4c23817d8b3f6b2ff39d1f9708230b78e767
Eugen Istoc <eugenistoc@gmail.com>
2024-01-05 18:50:32 -0500

    Add Nuxt UI Pro components and configurations

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: ["@nuxt/content", "@nuxt/ui", "@nuxtjs/google-fonts"],
+  devtools: { enabled: true },
+});
